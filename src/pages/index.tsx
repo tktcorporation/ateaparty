d5572363d1df59200a9a2aab11ba32b3455df299
tktcorporation <tktcorporation.go@gmail.com>
2021-06-08 05:15:48 +0900

    :sparkles: success to serve svg with `yarn serve`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import App from '../components/App'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <App>
-      <p>Index Page, Ueeeeeeeeeeeeeei</p>
+      <Image
+            src="/logo_yokogumi.svg"
+            alt="Ateapaty Logo"
+            width={1200}
+            height={300}
+          />
     </App>
   )
 }
