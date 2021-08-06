666936b6bcb8295549fcec3a1a65a6361efa2280
tktcorporation <tktcorporation.go@gmail.com>
2021-08-07 01:06:11 +0900

    :sparkles: set firebase analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import FirebaseProvider from "../../config/firebase";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+
+  return (
+    <>
+      <FirebaseProvider>
+        <Component {...pageProps} />
+      </FirebaseProvider>
+    </>
+  );
 }
 export default MyApp;
