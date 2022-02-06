bc4e38d29d1a5b454fb8d6b58dd5b3fdae411cc3
tktcorporation <tktcorporation.go@gmail.com>
2022-02-06 18:49:53 +0900

    :sparkles: downgrade firebase

--- config/firebase.tsx
+++ config/firebase.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useEffect, ReactNode } from "react";
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig =
   (process.env.NEXT_PUBLIC_FIREBASE_API_KEY &&
