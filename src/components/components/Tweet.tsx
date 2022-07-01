00550e20ec8c292a3cda7ebb05e32903e442458d
tkt <37575408+tktcorporation@users.noreply.github.com>
2022-07-01 16:16:41 +0000

    :sparkles: add a lazy component

---
+++ src/components/components/Tweet.tsx
@@ -0,0 +1,49 @@
+import { TwitterTweetEmbed } from "react-twitter-embed";
+import { usePromise } from "react-use";
+import { TwitterRepository } from "../../repository/TwitterRepository";
+const ACCOUNT_NAME = "ochakai_vrc";
+
+const wrapPromise = <T,>(
+  promise: Promise<T>
+): {
+  read(): T;
+} => {
+  let status = "pending";
+  let result: T | undefined;
+  const suspender = promise.then(
+    (r) => {
+      status = "success";
+      result = r;
+    },
+    (e) => {
+      status = "error";
+      result = e;
+    }
+  );
+  return {
+    read() {
+      //console.log(status);
+      if (status === "pending") {
+        throw suspender;
+      } else if (status === "success") {
+        if (result === undefined) {
+          throw result;
+        }
+        return result;
+      }
+      throw result;
+    },
+  };
+};
+
+const useTweetData = (accountName: string) => {
+  const repository = new TwitterRepository();
+  return wrapPromise(repository.getPinnedTweetId(accountName));
+};
+
+const SuspenseTwitterTweetEmbed = () => {
+  const pinnedTweetID = useTweetData(ACCOUNT_NAME).read();
+  return <TwitterTweetEmbed tweetId={pinnedTweetID} />;
+};
+
+export default SuspenseTwitterTweetEmbed;
