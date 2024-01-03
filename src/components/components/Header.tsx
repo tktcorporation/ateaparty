17a3b60526d16ec591cb5fd22949bc1cb9ff30c0
tkt <37575408+tktcorporation@users.noreply.github.com>
2024-01-03 15:47:20 +0000

    :recycle: use tailwindcss

--- src/components/components/Header.tsx
+++ src/components/components/Header.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Headroom from "react-headroom";
-import { Box, Link as RebassLink, Flex, Image } from "rebass/styled-components";
-import styled from "styled-components";
 import Link from "./Link";
 import { capitalize } from "../utils/string";
 import { BASE_URL, SECTION, TITLE } from "../utils/constants";
@@ -26,52 +24,36 @@ const Header = (): JSX.Element => {
   const { profile } = props;
 
   return (
-    <StyledHeadroom>
-      <Flex
-        flexWrap="wrap"
-        justifyContent="space-between"
-        alignItems="center"
-        px={3}
-      >
-        <RebassLink href={`#${getSectionHref(SECTION.home)}`} variant="empty">
-          <Flex justifyContent="center">
-            <Image
-              src={profile.bigIcon.src}
-              height={["60px", "80px"]}
-              width={["60px", "80px"]}
-              alt="Portfolio Logo"
-              p={2}
-              css={{ borderRadius: "20px", cursor: "pointer" }}
-            />
-          </Flex>
-        </RebassLink>
-        <Flex mr={[0, 3, 5]}>
+    <Headroom
+      className="absolute w-full transition-all"
+      wrapperStyle={{ width: "100%" }}
+      pinStart={0}
+      style={{ backgroundColor: theme.colors.primary }}
+    >
+      <div className="flex flex-wrap justify-between items-center px-3">
+        <a href={`#${getSectionHref(SECTION.home)}`} className="flex justify-center">
+          <img
+            src={profile.bigIcon.src}
+            height="80"
+            width="80"
+            alt="Portfolio Logo"
+            className="h-[60px] md:h-[80px] w-[60px] md:w-[80px] p-2 rounded-lg cursor-pointer"
+          />
+        </a>
+        <div className="flex mr-0 md:mr-3 lg:mr-5">
           {Object.keys(SECTION)
             .filter((id) => id !== "home")
             .map((id) => (
-              <Box key={id} ml={[2, 3]} color="background" fontSize={[2, 3]}>
+              <div key={id} className="ml-2 md:ml-3 text-background text-base md:text-lg">
                 <Link href={`#${id}`} tabIndex={0}>
                   {capitalize(id)}
                 </Link>
-              </Box>
+              </div>
             ))}
-        </Flex>
-      </Flex>
-    </StyledHeadroom>
+        </div>
+      </div>
+    </Headroom>
   );
 };
-
-const StyledHeadroom = styled(Headroom)`
-  * {
-    transition: background-color 0.1s ease;
-  }
-
-  .headroom--pinned {
-    background-color: ${theme.colors.primary};
-  }
-
-  position: absolute;
-  width: 100%;
-`;
 
 export default Header;
