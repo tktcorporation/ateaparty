2ae17a6812c7537da52ffba2e9a4364d01a60236
tkt <37575408+tktcorporation@users.noreply.github.com>
2024-01-05 12:23:08 +0000

    :art: remove unused dependencies

--- src/components/utils/script.ts
+++
@@ -1,7 +0,0 @@
-export const loadScript = (src: string): void => {
-  const tag = document.createElement("script");
-  tag.src = src;
-  tag.defer = true;
-
-  document.getElementsByTagName("body")[0].appendChild(tag);
-};
